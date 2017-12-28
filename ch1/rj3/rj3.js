a2654659724d579e482d2b7e1dc1320181d4e2c5
jslee <sungdrill@gmail.com>
2017-12-28 11:21:55 +0900

    init

---
+++ ch1/rj3/rj3.js
@@ -0,0 +1,53 @@
+var rj3 = {};
+
+rj3.svg = {};
+
+rj3.svg.line = function () {
+    var getX = function (point) {
+        return point[0];
+    },
+    getY = function(point) {
+        return point[1];
+    },
+    interpolate = function (points) {
+        return points.join("L");
+    };
+
+    function line(data) {
+        var segments = [],
+            points = [],
+            i = -1,
+            n = data.length,
+            d;
+
+        function segment() {
+            segments.push("M", interpolate(points));
+        }
+
+        while (++i < n) {
+            d = data[i];
+            points.push([+getX.call(this,d,i), +getY.call(this,d,i)]);
+        }
+
+        if (points.length) {
+            segment();
+        }
+
+        return segments.length ? segments.join("") : null;
+    }
+
+    line.x = function (funcToGetX) {
+        if (!arguments.length) return getX;
+
+        getX = funcToGetX;
+        return line;
+    };
+
+    line.y = function (funcToGetY) {
+        if (!arguments.length) return getY;
+        getY = funcToGetY;
+        return line;
+    };
+
+    return line;
+};
